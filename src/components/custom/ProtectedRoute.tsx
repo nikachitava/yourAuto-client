e7d5b94b534a014e434999b184a610db670f8c56
nikachitava <chitava18.official@gmail.com>
2024-11-05 15:43:36 +0400

    some configure change on ProtectedRoute component

--- src/components/custom/ProtectedRoute.tsx
+++ src/components/custom/ProtectedRoute.tsx
@@ -4,14 +4,20 @@ import { Navigate } from "react-router-dom";
 
 interface ProtectedRouteProps {
 	element: React.ComponentType;
+	redirectTo?: string;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
 	element: Component,
+	redirectTo = "/",
 }) => {
 	const { currentUser } = useContext(AuthorizationContext);
 
-	return currentUser ? <Component /> : <Navigate to="/" />;
+	if (currentUser === undefined) {
+		return <div>Loading...</div>;
+	}
+
+	return currentUser ? <Component /> : <Navigate to={redirectTo} />;
 };
 
 export default ProtectedRoute;
