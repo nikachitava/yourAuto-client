7323fb0a2bc6c4c1e438870a92fcbdbed8165ada
nikachitava <chitava18.official@gmail.com>
2024-11-22 17:39:15 +0400

    change base url for deploy

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const useAxios = axios.create({
     baseURL: "https://yourauto-server.onrender.com/",
+    
     // baseURL: "http://localhost:3000/",
     withCredentials: true
 })
